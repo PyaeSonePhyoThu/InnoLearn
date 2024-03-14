1c87ae5bd281da06f3323d4e5245fe24b71ab4bd
PyaeSonePhyoThu <pyaes0193@gmail.com>
2024-03-14 13:28:44 +0630

    Hello World

---
+++ App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hi");
+        System.out.println("Inno Learn");
+        System.out.print("Hi");
+    }
+}
